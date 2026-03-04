307ad2cd4bd12d18c43b536c45eb2eda22938631
Hans Maerki <buhtig.hans.maerki@ergoinfo.ch>
2026-03-04 15:55:09 +0100

    2026-03-04_sample_data/A 2

---
+++ 2026-03-04_sample_data/A/run_query.py
@@ -0,0 +1,39 @@
+""" """
+
+from __future__ import annotations
+
+import run_transform
+
+QUERY_SUMMARY = """
+FROM op_testoutcomes
+| LOOKUP JOIN op_testgroups ON id_group
+| EVAL is_passed = CASE(outcome == "passed", 1, 0),
+        is_failed = CASE(outcome == "failed", 1, 0)
+| STATS passed = SUM(is_passed), failed = SUM(is_failed) BY testgroup
+| SORT testgroup
+| LIMIT 1000
+"""
+
+QUERY_OUTCOMES= """
+FROM test-outcomes
+| LOOKUP JOIN testgroups ON testgroup_id
+| WHERE testgroup == "RUN-PERFBENCH"
+| KEEP test_name, tentacle, outcome
+| SORT test_name, tentacle
+  """
+def main() -> None:
+    client = run_transform.Elastic()
+
+    def table(query: str):
+
+        response = client.client.esql.query(query=query, format="txt")
+        print(response)
+
+    table(QUERY_SUMMARY)
+    table(QUERY_OUTCOMES)
+
+    client.close()
+
+
+if __name__ == "__main__":
+    main()
