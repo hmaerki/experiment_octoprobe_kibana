6ad9b1232582811bc68c07b50730faff6c9846b8
Hans Maerki <buhtig.hans.maerki@ergoinfo.ch>
2026-03-04 19:08:35 +0100

    2026-03-04_sample_data/A 6

--- 2026-03-04_sample_data/A/run_query.py
+++ 2026-03-04_sample_data/A/run_query.py
@@ -4,16 +4,6 @@ from __future__ import annotations
 
 import run_transform
 
-QUERY_SUMMARY = """
-FROM op_testoutcomes
-| LOOKUP JOIN op_testgroups ON id_group
-| EVAL is_passed = CASE(outcome == "passed", 1, 0),
-        is_failed = CASE(outcome == "failed", 1, 0)
-| STATS passed = SUM(is_passed), failed = SUM(is_failed) BY testgroup
-| SORT testgroup
-| LIMIT 1000
-"""
-
 QUERY_OUTCOMES = """
 FROM test-outcomes
 | LOOKUP JOIN testgroups ON testgroup_id
@@ -23,6 +13,25 @@ FROM test-outcomes
 | LIMIT 1000
 """
 
+QUERY_TESTRUNS = """
+FROM op_testruns
+| KEEP id_run
+| SORT id_run DESC
+| LIMIT 1000
+"""
+
+def query_summary(id_run: str) -> str:
+        return f"""
+FROM op_testoutcomes
+| LOOKUP JOIN op_testgroups ON id_group
+| LOOKUP JOIN op_testruns ON id_run
+| WHERE id_run == "{id_run}"
+| EVAL is_passed = CASE(outcome == "passed", 1, 0),
+        is_failed = CASE(outcome == "failed", 1, 0)
+| STATS passed = SUM(is_passed), failed = SUM(is_failed) BY testgroup
+| SORT testgroup
+| LIMIT 1000
+"""
 
 def main() -> None:
     client = run_transform.Elastic()
@@ -32,7 +41,11 @@ def main() -> None:
         response = client.client.esql.query(query=query, format="txt")
         print(response)
 
-    table(QUERY_SUMMARY)
+    response = client.client.esql.query(query=QUERY_TESTRUNS, format="json")
+    for row in response["values"]:
+        id_run = row[0]
+        print(f"*** {id_run=}")
+        table(query_summary(id_run=id_run))
     table(QUERY_OUTCOMES)
 
     client.close()
